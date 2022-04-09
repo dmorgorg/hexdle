0a72990f082125d1d549193ebd28d4a402b5bb70
Dave Morgan <dave@dmorg.org>
2022-04-08 19:09:46 -0600

    text render

--- src/App.svelte
+++ src/App.svelte
@@ -1,9 +1,7 @@
-<script>
-	export let name;
-</script>
+
 
 <main>
-	<h1>Hello {name}!</h1>
+	<h1> hexdle!</h1>
 	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
 </main>
 
