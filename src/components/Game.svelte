76660c1a1be551db0269a3492efe431cce8cbcfb
Dave Morgan <dave@dmorg.org>
2022-04-10 12:54:02 -0600

    render test

---
+++ src/components/Game.svelte
@@ -0,0 +1,26 @@
+<main>
+	<h1> hexdle!</h1>
+	
+</main>
+
+<style>
+	main {
+		text-align: center;
+		max-width: 640px;
+		margin: 0 auto;
+        background: pink;
+	}
+
+	h1 {
+		padding: 0;
+		text-transform: uppercase;
+		margin: 0;
+    }
+
+	@media (min-width: 640px) {
+		main {
+			/* max-width: none; */
+			border: 3px aqua solid;
+		}
+	}
+</style>
